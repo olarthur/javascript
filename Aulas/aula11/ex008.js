1d62452df3ff6c13fd7ac7af046795cbc8ff3af6
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-11-18 18:57:00 -0300

    Create ex008.js

---
+++ Aulas/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 45
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('Dirija usando cinto de segurança')

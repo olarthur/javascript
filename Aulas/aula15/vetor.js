4a4f08201dbf1be701c25273c28cc66ef337f93c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-11-22 18:05:54 -0300

    Create vetor.js

---
+++ Aulas/aula15/vetor.js
@@ -0,0 +1,12 @@
+let v = [2, 4, 5, 7, 9, 3]
+
+//Mostrar vetor em tela simplificado
+for (let p in v) {
+    console.log(`A posição ${p} tem o valor ${v[p]}`)
+}
+
+/*
+for (let p = 0; p < v.length; p++) {
+    console.log(`A posição ${p} tem o valor ${v[p]}`)
+}
+*/
